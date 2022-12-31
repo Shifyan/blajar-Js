4611747c26e6c36a94f00e398615b04f5bfabcbe
Shipppyaaannn <93694228+Shifyan@users.noreply.github.com>
2022-12-31 14:39:25 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,23 @@
+//syntax function//
+function iniFunction(a){
+    return
+}
+
+iniFunction()
+
+//contoh function//
+function tambah (a,b){
+    return a+b
+}
+console.log(tambah(2,3))
+
+function sapa (nama){
+    //var nama = nama
+    return `Halo ${nama} Selamat datang!`;
+}
+
+console.log(sapa('Shifyan'))
+console.log(sapa('Mustafid'))
+console.log(sapa('Ahmad'))
+console.log(sapa('Agus'))
+
