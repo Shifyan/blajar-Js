5a1943aaf4a6982468ca8ec75b4cdb204a26d295
Shipppyaaannn <93694228+Shifyan@users.noreply.github.com>
2022-12-31 15:57:54 +0700

    Add files via upload

---
+++ mcmfunction.js
@@ -0,0 +1,19 @@
+//terdapat 2 jenis function
+
+//function declaration & contoh
+
+function sebutNama(nama){
+    return `Halo ${nama}`
+}
+
+console.log(sebutNama('Shifyan'))
+
+
+var sapa = function (nama){
+    return `Halo ${nama}`
+}
+
+console.log(sapa('Ahmad'))
+
+
+
